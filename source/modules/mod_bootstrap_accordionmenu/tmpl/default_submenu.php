cbefa56723d9f8d06c35e1f8e56938e40a0c230e
Jisse Reitsma <info@yireo.com>
2015-10-23 10:51:13 +0200

    Allow new window also to work for subitems

--- source/modules/mod_bootstrap_accordionmenu/tmpl/default_submenu.php
+++ source/modules/mod_bootstrap_accordionmenu/tmpl/default_submenu.php
@@ -14,7 +14,7 @@ defined('_JEXEC') or die;
 <ul>
 	<?php foreach ($items as $item) : ?>
 		<li class="<?php echo implode(' ', $item->classes); ?>">
-			<a href="<?php echo $item->href; ?>"><?php echo $item->title; ?></a>
+			<a href="<?php echo $item->href; ?>" <?php if ($item->browserNav == 1) : ?>target="_new"<?php endif; ?>><?php echo $item->title; ?></a>
 			<?php if (!empty($item->childs)) : ?>
 				<?php $helper->submenu($item->childs); ?>
 			<?php endif; ?>
